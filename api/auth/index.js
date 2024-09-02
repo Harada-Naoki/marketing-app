d0fbf7b57128c1191dbb45ddbcc5681fdf3ebc75
Harada-Naoki <naoki_harada@eaglegroup.co.jp>
2024-09-02 14:02:12 +0900

    login2

--- api/auth/index.js
+++ api/auth/index.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // MongoDBに接続
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
