3bcd01ae2a60d784c4655501c91c10f40da3cb50
Harada-Naoki <naoki_harada@eaglegroup.co.jp>
2024-09-05 09:14:41 +0900

    ignore3

--- vercel-ignore-build-step.sh
+++ vercel-ignore-build-step.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_ENV: $VERCEL_ENV"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "frontend" ]] ; then
+if [[ "$VERCEL_ENV" == "frontend" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
-  exit 0;  # 続行するので exit 0 を使います
+  exit 1;
 
 else
   # Don't build
   echo "🛑 - Build cancelled"
-  exit 1;  # キャンセルするので exit 1 を使います
+  exit 0;
 fi
